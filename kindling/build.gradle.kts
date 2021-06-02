d2ef2cd96136d7a67b147fa676fb320e2bd0cf36
Richard Harrah <topplethenunnery@gmail.com>
2021-06-01 21:00:58 -0400

    chore(deps): update dependencies

--- kindling/build.gradle.kts
+++ kindling/build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     kotlin("jvm")
+    id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.5.0"
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:_")
+    implementation(kotlin("stdlib-jdk8"))
 
     testImplementation(platform("org.junit:junit-bom:_"))
     testImplementation("org.junit.jupiter:junit-jupiter")
